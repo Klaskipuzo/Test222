90822422cfecb25bf122fc279e8bf72a8fd29079
Menchkok <Klaskizuppi@gmail.com>
2022-05-11 21:34:03 +0700

    S1 - FINISH

--- Program.cs
+++ Program.cs
@@ -11,9 +11,7 @@ namespace MyProject2
         static void Main(string[] args)
         {
 
-
             
-
 
 
         }
@@ -24,12 +22,84 @@ namespace MyProject2
             Console.WriteLine(number);
 
         }
-        static void S11()
+
+        static void S1_2()
         {
             double a = Math.E;
             a = Math.Round(a, 1);
             Console.WriteLine(a);
         }
+        
+        static void S1_3_4()
+        {
+            Console.WriteLine("Введите число");
+
+            var number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Вы ввели число:{number}");
+        }
+
+        static void S1_5_6()
+        {
+            var number1 = 1;
+            var number2 = 13;
+            var number3 = 49;
+
+            Console.WriteLine($@"{number1} {number2} {number3}");
+            Console.WriteLine($@"{number1}  {number2}  {number3}");
+        }
+
+        static void S1_7()
+        {
+            Console.WriteLine("Введите три любых числа");
+            var number1 = int.Parse(Console.ReadLine());
+            var number2 = int.Parse(Console.ReadLine());
+            var number3 = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine($@"{number1}  {number2}  {number3}");
+        }
+
+        static void S1_8()
+        {
+            Console.WriteLine("Введите три любых числа");
+            var number1 = int.Parse(Console.ReadLine());
+            var number2 = int.Parse(Console.ReadLine());
+            var number3 = int.Parse(Console.ReadLine());
+            var number4 = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine($@"{number1} {number2} {number3} {number4}");
+        }
+
+        static void S1_9() {
+            var number1 = 10;
+            var number2 = 50;
+            Console.WriteLine($"{number2} \n {number1}");
+        }
+
+        static void S1_10() 
+        {
+            var number1 = 5;
+            var number2 = 10;
+            var number3 = 21;
+            Console.WriteLine($"{number2}\n{number1}\n{number3}");
+        }
+
+        static void S1_11() 
+        {
+            Random random = new Random();
+            Console.WriteLine("{0}\n{1}\n{2}\n{3}", random.Next(), random.Next(), random.Next(), random.Next());
+        } 
+
+        static void S1_12() 
+        {
+            Console.WriteLine($"{5} {10}\n{7}cm");
+        }
+
+        static void S1_13() 
+        {
             
+        }
     }
 }
