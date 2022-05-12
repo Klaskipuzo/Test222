134bf8eba93df1f16ae0bbfa005fbcccb899eec4
Menchkok <Klaskizuppi@gmail.com>
2022-05-12 21:11:59 +0700

    S2_2+

--- Program.cs
+++ Program.cs
@@ -10,14 +10,14 @@ namespace MyProject2
     {
         static void Main(string[] args)
         {
-
             
-
+                     
 
         }
 
         static void S1()
         {
+            //Вывести на экран число  с точностью до сотых.
             float number = 10.1234f;
             Console.WriteLine(number);
 
@@ -25,6 +25,7 @@ namespace MyProject2
 
         static void S1_2()
         {
+            //Вывести на экран число e (основание натурального логарифма) с точностью до десятых.
             double a = Math.E;
             a = Math.Round(a, 1);
             Console.WriteLine(a);
@@ -32,6 +33,9 @@ namespace MyProject2
         
         static void S1_3_4()
         {
+            //Составить программу вывода на экран числа, вводимого с клавиатуры. Выводимому числу должно предшествовать сообщение «Вы ввели число».
+            //Составить программу вывода на экран числа, вводимого с клавиатуры. После выводимого числа должно следовать сообщение » — вот какое число Вы  ввели».
+
             Console.WriteLine("Введите число");
 
             var number = int.Parse(Console.ReadLine());
@@ -41,6 +45,8 @@ namespace MyProject2
 
         static void S1_5_6()
         {
+            //Вывести на одной строке числа 1, 13 и 49 с одним пробелом между ними.
+            //Вывести на одной строке числа 7, 15 и 100 с двумя пробелами между ними.
             var number1 = 1;
             var number2 = 13;
             var number3 = 49;
@@ -51,6 +57,7 @@ namespace MyProject2
 
         static void S1_7()
         {
+            //Составить программу вывода на экран в одну строку трех любых чисел с двумя пробелами между ними.
             Console.WriteLine("Введите три любых числа");
             var number1 = int.Parse(Console.ReadLine());
             var number2 = int.Parse(Console.ReadLine());
@@ -62,6 +69,7 @@ namespace MyProject2
 
         static void S1_8()
         {
+            //Составить программу вывода на экран в одну строку четырех любых чисел  с одним пробелом между ними.
             Console.WriteLine("Введите три любых числа");
             var number1 = int.Parse(Console.ReadLine());
             var number2 = int.Parse(Console.ReadLine());
@@ -72,7 +80,9 @@ namespace MyProject2
             Console.WriteLine($@"{number1} {number2} {number3} {number4}");
         }
 
-        static void S1_9() {
+        static void S1_9() 
+        {
+            //Вывести на экран числа 50 и 10 одно под другим.
             var number1 = 10;
             var number2 = 50;
             Console.WriteLine($"{number2} \n {number1}");
@@ -80,6 +90,7 @@ namespace MyProject2
 
         static void S1_10() 
         {
+            //Вывести на экран числа 5, 10 и 21 одно под другим.
             var number1 = 5;
             var number2 = 10;
             var number3 = 21;
@@ -88,18 +99,42 @@ namespace MyProject2
 
         static void S1_11() 
         {
+            //Составить программу вывода на экран «столбиком» четырех любых чисел.
             Random random = new Random();
             Console.WriteLine("{0}\n{1}\n{2}\n{3}", random.Next(), random.Next(), random.Next(), random.Next());
         } 
 
         static void S1_12() 
         {
+            //Составить программу вывода на экран следующей информации:
+            //5 10
+            //7 см
+            
+            
             Console.WriteLine($"{5} {10}\n{7}cm");
         }
 
         static void S1_13() 
         {
-            
+            //Составить программу вывода на экран следующей информации:
+            // 2 кг
+            // 13 17
+            Console.WriteLine($"{2}KG \n{13} {17}");
+        }
+
+        static void S2_1()
+        {
+            Random random = new Random();
+            var y = 7 * Math.Pow(random.Next(1, 10), 2) + 3 * random.Next(1, 10) + 6;
+            Console.WriteLine(y);
+        }
+
+        static void S2_2()
+        {
+            Random random = new Random();
+            var x = 12 * Math.Pow(random.Next(1, 10), 2) + 7 * random.Next(1, 10) + 12;
+            Console.WriteLine(x);
         }
     }
+
 }
