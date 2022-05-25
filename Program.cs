ab914d3d9158d687f8efa97152fadabb133bc96a
Menchkok <Klaskizuppi@gmail.com>
2022-05-25 20:46:47 +0700

    25.05.46

--- Program.cs
+++ Program.cs
@@ -10,7 +10,25 @@ namespace MyProject2
     {
         static void Main(string[] args)
         {
-            
+            int[,,] mas = { { { 1, 2 },{ 3, 4 } },
+                { { 4, 5 }, { 6, 7 } },
+                { { 7, 8 }, { 9, 10 } },
+                { { 10, 11 }, { 12, 13 } } };
+
+
+            var rows = mas.GetUpperBound(0) + 1;
+            var cols = mas.Length / rows;
+
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write($"{mas[i,j]}\t");
+                }
+                Console.WriteLine();
+            }
+
 
         }
        
